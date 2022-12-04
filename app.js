d96506926c444b92de29d51048c57192d15674ab
rohan <rohansanjeev6464@gmail.com>
2022-12-04 08:26:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import selfcore from "selfcore";
 
 const client = new selfcore();
 const gateway = new selfcore.Gateway(
-  "NDA3OTIyNzMxNjQ1MDA5OTMy.GSMifh.tzyfJBKUblyC3OieOV32t_BTUStlaYPpX7ZdUA"
+  "_token_"
 );
 
 gateway.on("message", (m) => {
-  if (m.channel_id === "997566696137175144") {
+  if (m.channel_id === "_Channel_ID_") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
     client.sendWebhook(
-      "https://discord.com/api/webhooks/1038812589301432410/n3flhOTzw4XfsJq1WqkVaSOJuQPlVnymb13we66tHrCEe4dBL_4kifLNsKMnWav9ICHN",
+      "_Webhook_URL_",
       content
     );
   }
